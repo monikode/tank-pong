e2d67dce3574230e78bda7dd9c11376654585295
Gabriel <gdsl.snf21@uea.edu.br>
2022-03-13 13:07:26 -0400

    :construction: Hud
    :construction: Score Hud construction

--- screen.py
+++ screen.py
@@ -1,6 +1,6 @@
 import pygame
 
-from config import BG_COLOR, RECTS_COLOR, SCREEN_HEIGHT, SCREEN_RECTS, SCREEN_WIDTH, TOP_BAR_HEIGHT
+from config import BG_COLOR, RECTS_COLOR, SCREEN_HEIGHT, SCREEN_WIDTH, TANK_1_COLOR, TANK_2_COLOR, TOP_BAR_HEIGHT
 
 
 class Screen:
@@ -10,9 +10,18 @@ class Screen:
         self.surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.arena = self.surface.subsurface(
             (0, TOP_BAR_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT - TOP_BAR_HEIGHT))
+        self.font = pygame.font.Font("img/DSEG14Classic-Bold.ttf", 34)
 
-    def draw(self, map, tank1, tank2, balls):
+    def draw(self, map, score):
         self.surface.fill(BG_COLOR)
+
+        score_p1 = self.font.render(
+            str(score[0]), True, TANK_1_COLOR)
+        score_p2 = self.font.render(
+            str(score[1]), True, TANK_2_COLOR)
+
+        self.surface.blit(score_p1, (220, 20))
+        self.surface.blit(score_p2, (550, 20))
 
         for rect in map:
             pygame.draw.rect(self.arena, RECTS_COLOR, rect)
